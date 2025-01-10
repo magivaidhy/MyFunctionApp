e8d050ea0fddc2d13ee12ac168ca4ca69eceafb8
magivaidhy <84582372+magivaidhy@users.noreply.github.com>
2025-01-10 12:38:11 +0530

    Create init__.py

---
+++ init__.py
@@ -0,0 +1,22 @@
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Processing an HTTP request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            req_body = None
+        if req_body:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}!")
+    else:
+        return func.HttpResponse(
+            "Please pass a name in the query string or request body",
+            status_code=400
+        )
